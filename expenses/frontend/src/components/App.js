90d23fb14f572a3a91580c0588f5b1f5e7db8d6a
Wali <wali.rahman89@yahoo.com>
2020-04-15 21:38:06 -0400

    Added register and log in components and routes

--- expenses/frontend/src/components/App.js
+++ expenses/frontend/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import ReactDom from 'react-dom';
+import { HashRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 
 import { positions, Provider as AlertProvider } from 'react-alert';
 import AlertTemplate from 'react-alert-template-basic';
@@ -7,6 +8,9 @@ import AlertTemplate from 'react-alert-template-basic';
 import Header from './layout/Header';
 import Dashboard from './purchases/Dashboard';
 import Alerts from './layout/Alerts';
+import Login from './accounts/Login';
+import Register from './accounts/Register';
+
 
 import { Provider } from 'react-redux';
 import store from '../store';
@@ -23,15 +27,21 @@ class App extends Component {
         return (
             <Provider store={store}>
                 <AlertProvider template={AlertTemplate} {...alertOptions}>
-                    <Fragment>
-                        <Header />
-                        <Alerts />
-                        <div className="container">
-                            <Dashboard />
+                    <Router>
+                        <Fragment>
+                            <Header />
+                            <Alerts />
+                            <div className="container">
 
-                        </div>
+                                <Switch>
+                                    <Route exact path="/" component={Dashboard} />
+                                    <Route exact path="/register" component={Register} />
+                                    <Route exact path="/login" component={Login} />
+                                </Switch>
+                            </div>
 
-                    </Fragment>
+                        </Fragment>
+                    </Router>
                 </AlertProvider>
             </Provider>
 
--- expenses/frontend/src/components/layout/Header.js
+++ expenses/frontend/src/components/layout/Header.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from "react-router-dom"
 
 export class Header extends Component {
   render() {
@@ -10,6 +11,12 @@ export class Header extends Component {
         <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
           <a className="navbar-brand" href="#">Expenses</a>
           <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li className="nav-item">
+              <Link to="/register" className="nav-link">Register</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/login" className="nav-link">Login</Link>
+            </li>
 
           </ul>
         </div>
